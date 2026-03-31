0ed436a1c612c50d0437dd0e5aca5f1c0a8f8e63
Chong <0meoden0@gmail.com>
2026-03-31 16:01:12 +0700

    Lần commit đầu tiên - khởi tạo dự án

---
+++ Prime International Hotel/Prime International Hotel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prime_International_Hotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
